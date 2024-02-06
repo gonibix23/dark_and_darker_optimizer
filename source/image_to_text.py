61f9b8676b1c6c9ff41ccecbb788848cf25bf19b
gonibix23 <gonibix23@gmail.com>
2024-02-06 20:35:41 +0100

    Cambios

--- source/image_to_text.py
+++ source/image_to_text.py
@@ -12,12 +12,48 @@ def image_to_text(image_path):
     # Usa pytesseract para extraer texto de la imagen
     detected_text = pytesseract.image_to_string(image)
     # Regex para eliminar caracteres especiales
-    detected_text = re.sub('[^A-Za-z0-9|\\n|\.|\%|\,|-]+', ' ', detected_text)    
+    detected_text = re.sub('[^A-Za-z0-9|\\n|\.|\,|-]+', ' ', detected_text)    
     # A partir de la palabra Rarity, elimino el texto
     detected_text = detected_text.split('\n')
     detected_text = [x.strip() for x in detected_text]
     detected_text = list(filter(None, detected_text))
     
-    print(detected_text)
+    return detected_text
 
-image_to_text('items/test_04.png')
+def separate_text_and_number(string):
+    # Define a regular expression pattern to find text and numbers in the string
+    pattern = re.compile(r'([a-zA-Z\s]+)([+-]?\d*\.?\d+)')
+
+    # Search for matches in the string
+    matches = pattern.match(string)
+
+    if matches:
+        # Get the text and number from the matches
+        text = matches.group(1).strip()
+        number = float(matches.group(2))
+        return text, number
+    else:
+        # Define a regular expression pattern to find text and numbers in the string
+        pattern = re.compile(r'([+-]?\d*\.?\d+)([a-zA-Z\s]+)')
+
+        # Search for matches in the string
+        matches = pattern.match(string)
+        if matches:
+            # Get the text and number from the matches
+            text = matches.group(2).strip()
+            number = float(matches.group(1))
+            return text, number
+        else:
+            return "Empty", 0
+
+def text_to_item(text):
+    item = {
+        'Name': text[0]
+    }
+    for stat in text[1:]:
+        text, number = separate_text_and_number(stat)
+        item[text] = number
+    return item
+
+print(image_to_text('items/test_04.png'))
+print(text_to_item(image_to_text('items/test_04.png')))
